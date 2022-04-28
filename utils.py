84a744e5c847782e9812fd1fc67af92908566eea
Hen0k <henoktilaye7@gmail.com>
2022-04-28 19:30:14 +0300

    PEP8

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 from os import chdir
 import pandas as pd
 
+
 class DataLoader:
-    def __init__(self,dir_name,file_name):
+    def __init__(self, dir_name, file_name):
         self.dir_name = dir_name
         self.file_name = file_name
-
 
     def read_csv(self):
         chdir(self.dir_name)
